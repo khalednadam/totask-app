1ef4192ace4d09f7d0367e9643ec0dfa853df4f0
Khaled Nadam <khalednadam@gmail.com>
2024-05-06 10:23:54 +0300

    build

--- client/src/composables/socket.js
+++ client/src/composables/socket.js
@@ -1,4 +1,4 @@
 import { io } from "socket.io-client";
 const VITE_SERVER_URL = import.meta.env.VITE_SERVER_URL;
 // export const socket = io(`http://:3002/`);
-export const socket = io(`https://totask:3002.app/`);
+export const socket = io(`https://totask.app/`);

40d2f1a5d6ec1c864561a02fa376d6e813e5b814
Khaled Nadam <khalednadam@gmail.com>
2024-05-10 18:07:20 +0300

    fix: error messages

--- client/src/composables/helper.js
+++ client/src/composables/helper.js
@@ -7,10 +7,17 @@ const toast = useToast();
  * @param {Error} err - The error object containing the response data.
  */
 export const toastError = (err) => {
-  console.log(err);
-  const errorMessage = err.response.data;
-  const regex = /<pre>Error: (.*?)<br>/;
-  const match = errorMessage.match(regex);
-  const extractedErrorMessage = match ? match[1] : null;
-  toast.error(extractedErrorMessage)
+
+  let errorMessage = "An error occurred";
+
+  // Check if the error contains a response object and extract message from there
+  if (err.response && err.response.data && err.response.data.message) {
+    errorMessage = err.response.data.message;
+  } else if (err.message) {
+    // If no response object, try to get the message from the error object directly
+    errorMessage = err.message;
+  }
+
+  // Display the error message using toast
+  toast.error(errorMessage);
 }

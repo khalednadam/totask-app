de2226b84a6f2458af739a9520f558bac2732f49
Khaled Nadam <khalednadam@gmail.com>
2024-05-10 18:45:21 +0300

    fix: error messages

--- client/src/composables/helper.js
+++ client/src/composables/helper.js
@@ -7,17 +7,13 @@ const toast = useToast();
  * @param {Error} err - The error object containing the response data.
  */
 export const toastError = (err) => {
-  console.log(err);
   let errorMessage = "An error occurred";
 
-  // Check if the error contains a response object and extract message from there
   if (err.response && err.response.data && err.response.data.message) {
     errorMessage = err.response.data.message;
   } else if (err.message) {
-    // If no response object, try to get the message from the error object directly
     errorMessage = err.message;
   }
 
-  // Display the error message using toast
   toast.error(errorMessage);
 }
--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,14 +1,15 @@
 class ApiError extends Error {
-	constructor(statusCode, message, isOperational = true, stack = "") {
-		super(message);
-		this.statusCode = statusCode;
-		this.isOperational = isOperational;
-		if (stack) {
-			this.stack = stack;
-		} else {
-			Error.captureStackTrace(this, this.constructor);
-		}
-	}
+  constructor(statusCode, message, isOperational = true, stack = "") {
+    super(message);
+    this.statusCode = statusCode;
+    this.message = message; // Set the message property
+    this.isOperational = isOperational;
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
 }
 
 module.exports = ApiError;

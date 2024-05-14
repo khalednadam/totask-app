26f36a3b7a63791443fb2d7ad943e2f365f05f86
Khaled Nadam <khalednadam@gmail.com>
2024-05-14 23:36:05 +0300

    feat: user card

---
+++ client/src/components/Admin/UserCard.vue
@@ -0,0 +1,37 @@
+<script setup>
+import UserAvatar from "../UserAvatar.vue";
+const props = defineProps({
+  user: Object
+})
+
+</script>
+<template>
+  <v-card>
+    <v-card-text>
+      <div class="flex items-center gap-2">
+        <UserAvatar :user="user" />
+        <div>
+          <h2>
+            {{ user.name }}
+          </h2>
+          <p class="opacity-60 text-xs">
+            @{{ user.username }}
+          </p>
+        </div>
+      </div>
+      <div>
+        <p>
+          email: {{ user.email }}
+        </p>
+        <p>
+          registerd on: {{ new Date(user.createdAt).toLocaleString("en-GB", {
+            day: "2-digit", month: "short", year:
+              "numeric"
+          }) }}
+        </p>
+      </div>
+    </v-card-text>
+  </v-card>
+</template>
+
+

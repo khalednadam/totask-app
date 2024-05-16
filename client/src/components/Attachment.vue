af2e897c6e5e2e1485bace4f41dee2c3af0499b2
Khaled Nadam <khalednadam@gmail.com>
2024-05-16 21:46:54 +0300

    fix: comment download functionality

--- client/src/components/Attachment.vue
+++ client/src/components/Attachment.vue
@@ -33,26 +33,7 @@ const deleteAttachment = () => {
     console.log(err);
   })
 }
-const downloadFile = async (uri) => {
-  try {
-    const fileUri = uri; // Replace 'YOUR_FILE_URI' with the actual URI of the file in your Google Cloud Storage bucket
-    const response = await axiosInstance(fileUri, {
-      // headers: {
-      //   'Access-Control-Allow-Credentials': true
-      // }
-    });
-    const blob = await response.blob();
-    const blobUrl = window.URL.createObjectURL(blob);
-    const link = document.createElement('a');
-    link.href = blobUrl;
-    link.download = 'filename'; // Optionally set the downloaded file name
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-  } catch (error) {
-    console.error('Error downloading file:', error);
-  }
-}
+
 </script>
 <template>
   <v-row>
@@ -60,7 +41,7 @@ const downloadFile = async (uri) => {
       <v-img :src rounded="lg">
       </v-img>
       <div v-if="src.slice(-3) === 'pdf'" class="h-24">
-        <a :href="`${encodedURL}`" :download="filename">
+        <a :href="`${encodedURL}`">
           <v-avatar color="primary" rounded="lg" size="100%" class="w-full bg-red">
             PDF
           </v-avatar>
@@ -76,15 +57,15 @@ const downloadFile = async (uri) => {
       </p>
       <div class="flex gap-1 justify-start items-center">
         <p class="text-xs cursor-pointer underline">
-          <!-- <a :href="`${encodedURL}`" rel="external" target="_blank"> -->
-          <!--   View -->
-          <!-- </a> -->
+          <a :href="`${encodedURL}`" rel="external" target="_blank">
+            View
+          </a>
         </p>
-        <p class="text-xs underline cursor-pointer">
-        <div @click="() => downloadFile(encodedURL)" :download="filename">
-          Download
-        </div>
-        </p>
+        <!-- <p class="text-xs underline cursor-pointer"> -->
+        <!-- <div @click="() => downloadFile(encodedURL)" :download="filename"> -->
+        <!--   Download -->
+        <!-- </div> -->
+        <!-- </p> -->
         <p class="text-xs underline cursor-pointer" @click="deleteAttachment">
           Delete
         </p>

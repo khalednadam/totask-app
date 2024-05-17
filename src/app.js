400308b3c18439cd54c27b11321fae13ca8e91f2
Khaled Nadam <khalednadam@gmail.com>
2024-05-17 23:07:00 +0300

    test: error msg

--- src/app.js
+++ src/app.js
@@ -73,7 +73,15 @@ app.use(
     store: MongoStore.create({ mongoUrl: config.mongoose.url }),
   })
 );
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
 
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message
+  });
+});
 
 
 app.use(passport.session());

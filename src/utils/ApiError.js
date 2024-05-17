c456a9ff1c40081d633043e40b7a0efe54413bae
Khaled Nadam <khalednadam@gmail.com>
2024-05-17 22:58:30 +0300

    test: error message

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,15 +1,23 @@
+// class ApiError extends Error {
+//   constructor(statusCode, message, isOperational = true, stack = "") {
+//     super(message);
+//     this.statusCode = statusCode;
+//     this.message = message; // Set the message property
+//     this.isOperational = isOperational;
+//     if (stack) {
+//       this.stack = stack;
+//     } else {
+//       Error.captureStackTrace(this, this.constructor);
+//     }
+//   }
+// }
 class ApiError extends Error {
-  constructor(statusCode, message, isOperational = true, stack = "") {
+  constructor(statusCode, message) {
     super(message);
     this.statusCode = statusCode;
-    this.message = message; // Set the message property
-    this.isOperational = isOperational;
-    if (stack) {
-      this.stack = stack;
-    } else {
-      Error.captureStackTrace(this, this.constructor);
-    }
+    this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
+    this.isOperational = true;
+    Error.captureStackTrace(this, this.constructor);
   }
 }
-
 module.exports = ApiError;

ec9c71215e9cd77649d64da8ea5b1d8b72b17a5a
Khaled Nadam <khalednadam@gmail.com>
2024-05-22 12:37:23 +0300

    fix: remove card skeleton loader

--- client/src/components/List.vue
+++ client/src/components/List.vue
@@ -318,20 +318,20 @@ watch(cards, () => {
     <!-- VueDraggablePlus -->
     <div class="overflow-hidden flex-col flex max-h-[70vh]">
       <div class="overflow-y-auto px-2 flex-1 max-h-full">
-        <div v-if="isLoading || isListLoading">
-          <template v-for="i in Math.floor(Math.random() * 3) + 1" :key="i">
-            <v-skeleton-loader class="my-3" type="card-avatar"></v-skeleton-loader>
-          </template>
-        </div>
+        <!-- <div v-if="isLoading || isListLoading"> -->
+        <!--   <template v-for="i in Math.floor(Math.random() * 3) + 1" :key="i"> -->
+        <!--     <v-skeleton-loader class="my-3" type="card-avatar"></v-skeleton-loader> -->
+        <!--   </template> -->
+        <!-- </div> -->
         <!-- <v-infinite-scroll max-height="60vh" height="max" :items="cards" :onLoad="getCards"> -->
         <!--   <template v-slot:empty> -->
         <!--     <p> -->
         <!--       s -->
         <!--     </p> -->
         <!--   </template> -->
-        <VueDraggable ref="el" group="cards" class="space-y-3 relative" v-model="cards" :animation="150" dragClass="drag"
-          ghostClass="ghost" @update="onUpdate" scroll :scrollSensitivity="300" bubbleScroll @add="onAdd"
-          v-if="!isLoading">
+        <VueDraggable ref="el" group="cards" class="space-y-3 relative" v-model="cards" :animation="150"
+          dragClass="drag" ghostClass="ghost" @update="onUpdate" scroll :scrollSensitivity="300" bubbleScroll
+          @add="onAdd" :key="isLoading">
           <template v-for="card in cards" :key="card.id">
             <Card :listName="list.name" :card @delete-card="(cardId) => deleteCard(cardId)" />
           </template>

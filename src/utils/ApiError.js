4556290f75aa5dce2f11c821df1ffbc9fc16ef04
Khaled Nadam <khalednadam@gmail.com>
2024-05-23 15:06:39 +0300

    fix: api error

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,8 +1,7 @@
 class ApiError extends Error {
-  constructor(statusCode, message, isOperational = true, stack = "") {
+  constructor(statusCode, message, isOperational = true, stack = '') {
     super(message);
     this.statusCode = statusCode;
-    this.message = message; // Set the message property
     this.isOperational = isOperational;
     if (stack) {
       this.stack = stack;

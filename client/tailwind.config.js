5944531200fe0363dda41c43e27772139fadbbcb
Khaled Nadam <khalednadam@gmail.com>
2024-08-07 16:59:52 +0300

    fix(landing): fix styles

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,6 +4,22 @@ export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      animation: {
+        tilt: 'tilt 5s infinite linear'
+      },
+      keyframes: {
+        tilt: {
+          "0%, 50%, 100%": {
+            transform: "rotate(0deg)"
+          },
+          "25%": {
+            transform: "rotate(5deg)"
+          },
+          "75%": {
+            transform: "rotate(-5deg)"
+          },
+        }
+      },
       colors: {
         primary: "#6DB193",
         secondary: "#FF5959",

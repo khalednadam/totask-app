84f6103f6f5efe823c7e7f3d8e1fa9a4e05c773e
Khaled Nadam <khalednadam@gmail.com>
2024-09-02 18:08:39 +0300

    fix: :bug: card cover buttons

--- client/src/components/CardCover.vue
+++ client/src/components/CardCover.vue
@@ -3,6 +3,7 @@ import { toastError } from "@/composables/helper.js";
 import axiosInstance from "@/composables/axios";
 import { ref } from "vue";
 import { socket } from "../composables/socket";
+import { Icon } from "@iconify/vue/dist/iconify.js";
 
 const cardCover = defineModel();
 
@@ -43,67 +44,79 @@ const deleteCardCover = () => {
 </script>
 <template>
   <div>
-    <v-hover v-slot="{ isHovering, props }" :open-delay="200">
-      <v-img v-bind="props" v-if="cover" :src="cover" cover height="200">
-        <v-overlay
-          :model-value="isHovering"
-          scrim="black"
-          class="align-end gap-2 flex space-x-4 justify-end"
-          contained
-        >
-          <div class="flex justify-end gap-2 mb-1 mr-1">
-            <v-btn color="error" size="small" @click="deleteCardCover">
-              Delete Cover
-            </v-btn>
-            <v-btn
-              v-bind="props"
-              @click="changeCoverMenu = true"
-              variant="flat"
-              color="primary"
-              size="small"
-            >
-              Change Cover
-            </v-btn>
-          </div>
-        </v-overlay>
-        <v-menu
-          class="mx-auto -mt-48 flex justify-center items-center"
-          v-model="changeCoverMenu"
-        >
-          <template v-slot:default="{ isActive }">
-            <v-card class="w-80">
-              <v-card-text>
-                <v-file-input
-                  v-model="cardCover"
-                  accept="image/*"
-                  label="Cover"
-                  variant="solo-filled"
-                ></v-file-input>
-              </v-card-text>
-              <v-card-actions
-                class="flex justify-end self-end justify-self-end"
+    <v-img
+      v-bind="props"
+      v-if="cover"
+      :src="cover"
+      cover
+      height="200"
+      class="flex justify-end items-end"
+    >
+      <div
+        class="align-end gap-2 flex space-x-4 py-1 px-4 justify-end bg-gradient-to-t from-black/40 via-black/20 to-transparent"
+      >
+        <div class="flex justify-end gap-2 mb-1 mr-1">
+          <v-tooltip text="Delete Cover" :open-delay="400">
+            <template v-slot:activator="{ props }">
+              <v-btn
+                v-bind="props"
+                color="error"
+                icon
+                size="small"
+                @click="deleteCardCover"
               >
-                <v-btn
-                  variant="outlined"
-                  color="primary"
-                  @click="isActive.value = false"
+                <Icon icon="ph:trash" width="20" />
+              </v-btn>
+            </template>
+          </v-tooltip>
+          <v-menu class="mx-auto -mt-48 flex justify-center items-center">
+            <template v-slot:activator="{ props }">
+              <v-btn
+                v-bind="props"
+                @click="changeCoverMenu = true"
+                variant="flat"
+                color="primary"
+                size="small"
+                icon
+              >
+                <Icon icon="ph:pencil" width="20" />
+              </v-btn>
+            </template>
+            <template v-slot:default="{ isActive }">
+              <v-card class="w-80">
+                <v-card-text>
+                  <v-file-input
+                    v-model="cardCover"
+                    accept="image/*"
+                    label="Cover"
+                    variant="solo-filled"
+                  ></v-file-input>
+                </v-card-text>
+                <v-card-actions
+                  class="flex justify-end self-end justify-self-end"
                 >
-                  Cancel
-                </v-btn>
-                <v-btn
-                  @click="emit('addCardCover')"
-                  :disabled="isLoading"
-                  :loading="isLoading"
-                  variant="flat"
-                  color="primary"
-                >
-                  Upload
-                </v-btn>
-              </v-card-actions>
-            </v-card>
-          </template>
-        </v-menu>
-      </v-img>
-    </v-hover>
+                  <v-btn
+                    variant="outlined"
+                    color="primary"
+                    @click="isActive.value = false"
+                  >
+                    Cancel
+                  </v-btn>
+                  <v-btn
+                    @click="emit('addCardCover')"
+                    :disabled="isLoading"
+                    :loading="isLoading"
+                    variant="flat"
+                    color="primary"
+                  >
+                    Upload
+                  </v-btn>
+                </v-card-actions>
+              </v-card>
+            </template>
+          </v-menu>
+        </div>
+      </div>
+    </v-img>
   </div>
 </template>
